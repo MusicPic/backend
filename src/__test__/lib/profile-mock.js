bed55adcb1795c7b3b240b3b5ec741beded38f18
Joanna Coll <johannam999@gmail.com>
2018-06-18 14:33:39 -0700

    testing profile

---
+++ src/__test__/lib/profile-mock.js
@@ -0,0 +1,32 @@
+'use strict';
+
+import faker from 'faker';
+import Profile from '../../models/profile';
+import { createAccountMock, removeAccountMock } from './account-mock';
+
+const createProfileMock = () => {
+  const resultMock = {};
+
+  return createAccountMock()
+    .then((accountSetMock) => {
+      resultMock.accountSetMock = accountSetMock;
+      return new Profile({
+        username: faker.name.word(),
+        avatar: faker.random.image(),
+        account: accountSetMock.account._id,
+      }).save();
+    })
+    .then((profile) => {
+      resultMock.profile = profile;
+      return resultMock;
+    });
+};
+
+const removeProfileMock = () => {
+  return Promise.all([
+    Profile.remove({}),
+    removeAccountMock(),
+  ]);
+};
+
+export { createProfileMock, removeProfileMock };
