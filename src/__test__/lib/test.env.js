0271f42631daaa77277289909558c8927192e3f3
sarasaurus <bixler.s.e@gmail.com>
2018-06-18 16:19:31 -0700

    .gitignore

--- src/__test__/lib/test.env.js
+++ src/__test__/lib/test.env.js
@@ -2,4 +2,4 @@ process.env.NODE_ENV = 'development';
 process.env.PORT = 7000;
 process.env.MONGODB_URI = 'mongodb://localhost/musicpic';
 
-require('dotenv').config();
+// require('dotenv').config();
