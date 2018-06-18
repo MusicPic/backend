f0fba0026fc003e189caf7f5135ba4ef34a329c3
sarasaurus <bixler.s.e@gmail.com>
2018-06-18 16:22:08 -0700

    hm

--- src/__test__/lib/test.env.js
+++ src/__test__/lib/test.env.js
@@ -2,4 +2,6 @@ process.env.NODE_ENV = 'development';
 process.env.PORT = 7000;
 process.env.MONGODB_URI = 'mongodb://localhost/musicpic';
 
-// require('dotenv').config();
+require('dotenv').config();
+
+// hey you
