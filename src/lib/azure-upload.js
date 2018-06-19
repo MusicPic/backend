ee7c0516f625c83ad4aa3eeff34b9741205e81db
sarasaurus <bixler.s.e@gmail.com>
2018-06-19 10:56:54 -0700

    clean up azure module

--- src/lib/azure-upload.js
+++ src/lib/azure-upload.js
@@ -4,44 +4,10 @@
 const superagent = require('superagent');
 require('dotenv').config();
 
-// const subscriptionKey = process.env.AZURE_KEY;
-
-// const uriBase = process.env.URI_BASE;
-
 const trialUrl = 'https://i.imgflip.com/vh6to.jpg';
 
-// const params = {
-//   returnFaceId: 'true',
-//   returnFaceLandmarks: 'false',
-//   returnFaceAttributes: 'emotion',
-// };
-
-// const options = {
-//   uri: uriBase,
-//   qs: params,
-//   body: `{"url": "${imageUrl}"}`,
-//   headers: {
-//     'Content-Type': 'application/json',
-//     'Ocp-Apim-Subscription-Key': subscriptionKey,
-//   },
-// };
-
 const azureUpload = (imageUrl) => {
-  // const params = {
-  //   returnFaceId: 'true',
-  //   returnFaceLandmarks: 'false',
-  //   returnFaceAttributes: 'emotion',
-  // };
-  // const options = {
-  //   uri: process.env.URI_BASE,
-  //   qs: params,
-  //   body: `{"url": "${imageUrl}"}`,
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'Ocp-Apim-Subscription-Key': process.env.AZURE_KEY,
-  //   },
-  // };
-  return superagent.post(`${process.env.URI_BASE}`)
+  return superagent.post(process.env.URI_BASE)
     .query({ 
       returnFaceId: 'true',
       returnFaceLandmarks: 'false',
@@ -51,7 +17,6 @@ const azureUpload = (imageUrl) => {
     .set('Ocp-Apim-Subscription-Key', process.env.AZURE_KEY)
     .send(`{"url": "${imageUrl}"}`)
     .then((response) => {
-      console.log('DATA body', response.body[0].faceAttributes.emotion);
       const emotionData = response.body[0].faceAttributes.emotion;
       const getMax = (object) => {
         return Object.keys(object).filter((x) => {
@@ -59,34 +24,14 @@ const azureUpload = (imageUrl) => {
         });
       };
       const spotifySearchTerm = getMax(emotionData);
-      console.log('spotify search term', spotifySearchTerm[0]);
       return spotifySearchTerm[0];
     })
     .catch((err) => {
-      console.log('ERR', err);
+      throw err;
     });
-    
-    
-  //   options, (error, response, body) => {
-  //   if (error) {
-  //     console.log('Error: ', error);
-  //     return;
-  //   }
-  //   const parsedJSON = JSON.parse(body);
-  //   const emotionData = parsedJSON[0].faceAttributes.emotion;
-  //   console.log('EMOTION DATA', emotionData);
-  //   const getMax = (object) => {
-  //     return Object.keys(object).filter((x) => {
-  //       return object[x] === Math.max.apply(null, Object.values(object));
-  //     });
-  //   };
-  //   const spotifySearchTerm = getMax(emotionData);
-  //   console.log('spotify search term', spotifySearchTerm[0]);
-  //   // return spotifySearchTerm[0];
-  // });
 };
+
 azureUpload(trialUrl)
   .then(response => console.log('response', response))
-  .catch(err => console.log('error', err));
-// console.log('function return', azureUpload(trialUrl));
+  .catch(err => console.log('error!', err.status));
 module.exports.azureUpload = azureUpload;
