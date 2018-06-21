46779254384c622e744a90097bd937da1796a412
sarasaurus <bixler.s.e@gmail.com>
2018-06-21 09:25:20 -0700

    cleaned up comments and logs

--- src/routes/picture-route.js
+++ src/routes/picture-route.js
@@ -14,14 +14,13 @@ import logger from '../lib/logger';
 
 const jsonParser = json();
 const multerUpload = multer({ dest: `${__dirname}/../temp` });
-
+// multer attaches a file property to the request object
 const pictureRouter = new Router();
-// multer attaches a files propert to the request object, multerUpload.single(feildname)- feildname being a string, i think is what we want
 
 pictureRouter.post('/picture', bearerAuthMiddleware, multerUpload.single('thePicture'), jsonParser, (request, response, next) => {
-  console.log('REQUEST FILE', request.file);
-  console.log('REQUEST PICTURE PATH', request.file.path);
-  console.log('ACCOUNT INFO', request.account._id);
+  logger.log(logger.INFO, 'REQUEST FILE', request.file);
+  logger.log(logger.INFO, 'REQUEST PICTURE PATH', request.file.path);
+  logger.log(logger.INFO, 'ACCOUNT INFO', request.account._id);
   if (!request.account._id) {
     return next(new HttpError(404, 'ASSET ROUTER ERROR: asset not found, no account! '));
   }
@@ -30,28 +29,21 @@ pictureRouter.post('/picture', bearerAuthMiddleware, multerUpload.single('thePic
   }
   const picture = request.file;
   const key = `${picture.filename}.${picture.originalname}`;
-  // const options = { runValidators: true, new: true };
-  // const theProfile = profile;
+
   return s3Upload(picture.path, key)
     .then((url) => {
-      console.log('URL AFTER AWS', url);
-      // response.body.url = url;
-      // logger.log(logger.INFO, 'SAVING URL', response.body.url);
+      logger.log(logger.INFO, 'URL AFTER AWS', url);
       return azureUpload(url)
         .then((keyword) => {
           logger.log(logger.INFO, 'KEYWORD AFTER AZURE', keyword);
-          // response.body.keyword = keyword;
-          // logger.log(logger.INFO, 'SAVING PITCTURE DATA', response.body.keyword);
-          // to reconfigure with AWS would need to first upload picture to AWS, then save thPicture to DB and then use url property from db to call azure
+
           return Picture.create(
             keyword, 
             url, 
           );
         })
         .then((newPicture) => {
-          // logger.log(logger.INFO, 'PICTURE RETURNING', response.body);
           logger.log(logger.INFO, 'MONGO PICTURE', newPicture);
-          // request.body contains, keyword and url properties
           return response.json(newPicture);
         });
     })
